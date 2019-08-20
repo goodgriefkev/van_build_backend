4a283dd0bfabf15c286a057160871ee10f81838b
Kevin Honnoll <khonnoll@gmail.com>
2019-08-19 23:37:05 -0400

    installed body-parser, required and added to middleware in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -11,16 +12,20 @@ const vanbuildsController = require('./controllers/vanbuilds.js')
 
 // // Mongoose setup // //
 
-mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
-mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'));
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
 
-mongoose.connect('mongodb://localhost:27017/holidays', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/holidays', { useNewUrlParser: true });
 mongoose.connection.once('open', ()=>{
-    console.log('connected to mongoose...')
+    console.log('connected to mongoose...');
 
 
 // // Middleware // //
-app.use('/vanbuilds', vanbuildsController)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use('/vanbuilds', vanbuildsController);
 
 app.listen(PORT, () => {
   console.log('listening on port', PORT)
