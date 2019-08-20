3646912ad5e347195fb09200a1aca47aa4de213f
Kevin Honnoll <khonnoll@gmail.com>
2019-08-20 15:43:43 -0400

    updated users migration schema

--- migrations/20190820154034_create-users.js
+++ migrations/20190820154034_create-users.js
@@ -6,9 +6,9 @@ exports.up = function(knex, Promise) {
     table.string('password').notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
-  })
+  });
 };
 
-exports.down = function(knex) {
-
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
 };
