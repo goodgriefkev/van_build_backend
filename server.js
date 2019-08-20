3b0642f0865fd1e4b7b7c5c5e9560b0833aba442
Kevin Honnoll <khonnoll@gmail.com>
2019-08-20 10:28:20 -0400

    uninstalled mongoose and removed from code

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -9,15 +8,6 @@ const app = express();
 const PORT = process.env.PORT;
 
 const vanbuildsController = require('./controllers/vanbuilds.js')
-
-// // Mongoose setup // //
-
-mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'));
-mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
-
-mongoose.connect('mongodb://localhost:27017/holidays', { useNewUrlParser: true });
-mongoose.connection.once('open', ()=>{
-    console.log('connected to mongoose...');
 
 
 // // Middleware // //
