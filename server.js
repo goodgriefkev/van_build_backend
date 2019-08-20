4d7bec6c91723c77006d94d41b852f9e9b70e2d2
Kevin Honnoll <khonnoll@gmail.com>
2019-08-19 21:24:32 -0400

    installed mongoose, required in server.js, added connection messaging

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
 const PORT = process.env.PORT;
 
 const vanbuildsController = require('./controllers/vanbuilds.js')
 
+// // Mongoose setup // //
+
+mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+
+mongoose.connect('mongodb://localhost:27017/holidays', { useNewUrlParser: true })
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongoose...')
+
+
+// // Middleware // //
 app.use('/vanbuilds', vanbuildsController)
 
 app.listen(PORT, () => {
