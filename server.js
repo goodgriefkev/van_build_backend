807efe52ac2331e86fadf96a71eddd068a3acb53
Kevin Honnoll <khonnoll@gmail.com>
2019-08-24 11:29:46 -0400

    required bcrypt in server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
